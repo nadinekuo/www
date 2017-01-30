8260eb3290b29fae9f64e07e68d92176582b502b
Frank Löffler <knarf@cct.lsu.edu>
2017-01-30 13:01:01 -0600

    move viewport meta out of include

--- head.js
+++ head.js
@@ -1,10 +1,9 @@
 var root = [].slice.call(document.getElementsByTagName("script")).pop().src.split("/").slice(0,-1).join("/")+"/";
 
 document.write(
-  '<meta content="text/html; charset=utf-8" http-equiv="Content-Type">'+
   '<script src="'+root+'bootstrap/js/jquery.min.js"></script>'+
   '<script src="'+root+'bootstrap/js/bootstrap.min.js"></script>'+
-  '<meta content="initial-scale=1" name="viewport">'+
+  '<meta name="viewport" content="width=device-width, initial-scale=1">'+
   '<!-- Latest compiled and minified CSS -->'+
   '<link href="'+root+'bootstrap/css/bootstrap.min.css" rel="stylesheet">'+
   '<link href="'+root+'bootstrap/css/bootstrap-theme.min.css" rel="stylesheet">'+
