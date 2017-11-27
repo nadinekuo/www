bcf35250555ae28705ade3b61be797de7fdfebba
Steven R. Brandt <sbrandt@cct.lsu.edu>
2017-11-27 10:25:54 -0600

    Attempting to fix accents, try 2

--- send-registration.php
+++ send-registration.php
@@ -18,7 +18,7 @@
 
 <?php
 /* All form fields are automatically passed to the PHP script through the array $HTTP_POST_VARS. */
-$name = $_POST['name'];
+$name = htmlentities($_POST['name'], ENT_QUOTES, "UTF-8");
 $email = $_POST['email'];
 $home = $_POST['home'];
 $institution = $_POST['institution'];
