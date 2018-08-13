83c8aa504096eef925c431080ff0591a1a9aea66
Roland Haas <rhaas@ncsa.illinois.edu>
2018-08-13 11:36:22 -0500

    add CGI script to display possible unanswered questions

---
+++ tools/unanswered.php
@@ -0,0 +1,32 @@
+<html lang="en">
+<head>
+  <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+  <script src="head.js" type="text/javascript">
+  </script>
+  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+  <title>Possible unaswered emails on users mailing list</title>
+</head>
+<body id="index">
+  <header>
+    <script src="menu.js" type="text/javascript"></script>
+  </header>
+  <div class="container">
+<?php
+$title='Possible unaswered emails on users mailing list';
+$hide_path=1;
+$category='internal';
+
+echo '<h3>Possible unanswered emails</h3>';
+echo system('unanswered.pl');
+?>
+  </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <script src="footer/footer.js" type="text/javascript"></script>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
+
---
+++ tools/unanswered.pl
@@ -0,0 +1,89 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use LWP::Simple;
+
+# some constants (do not use locale specific values since the server is in the
+# US no matter what my locale is).
+my @months = ("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+my $url_fmt = "http://lists.einsteintoolkit.org/pipermail/users/%04d-%s/date.html";
+
+# these hold the list of emails and the root of the conversations
+our (%emails, %roots, %tails);
+
+# get emails logs for the three months braketing the current one
+my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime();
+for (my $m = -1 ; $m <= 1 ; $m++) {
+  my $monthyear = $year * 12 + $mon + $m;
+  my $monthname = $months[$monthyear % 12];
+  my $fullyear = 1900 + int($monthyear / 12);
+  my $url = sprintf($url_fmt, $fullyear, $monthname);
+  my $content = get($url);
+  parse_content($content, $url) if $content;
+} 
+
+# if there is an email thread with either only a single post or where the last
+# poster is the original poster then they are candidates for unanswered emails
+print "<ul>\n";
+foreach my $key (sort keys %emails) {
+  my @authors = @{$emails{$key}};
+  my $num_authors = scalar @authors;
+  if($num_authors == 1 or $authors[0] eq $authors[-1]) {
+    if($num_authors == 1 and 
+       $authors[0] eq "Roland Haas" and
+       $key =~ m/\[Users\] meeting minutes for/) {
+     next;
+    }
+    print "<li>".sanitize("$key ($authors[0])").": <a href='".url($roots{$key})."'>root</a>";
+    print " <a href='".url($tails{$key})."'>tail</a>" if $num_authors > 1;
+    print "</li>\n";
+  }
+}
+print "</ul>\n";
+
+sub parse_content {
+  my ($content, $monthurl) = @_;
+  our (%emails, $roots);
+
+  $monthurl =~ s!/[^/]*$!!;
+
+  my @lines = split /\n/,$content;
+  my $subject;
+  foreach (@lines) {
+    if(/^<LI><A HREF="(\d*\.html)">(.*)/) {
+      my $url = $1;
+      $subject = $2;
+      # apparently some subjects have random whitespace (maybe line breaking?)
+      $subject =~ s/\s\s*/ /g;
+      if(not exists $emails{$subject}) {
+        $emails{$subject} = [];
+        $roots{$subject} = $monthurl . "/" . $url;
+      }
+      $tails{$subject} = $monthurl . "/" . $url;
+    } elsif(/^<I>(.*)/) {
+      my $sender = $1;
+      push @{$emails{$subject}}, ($sender);
+    }
+  }
+}
+
+sub sanitize {
+  # a very trivial text sanitizer to remove all html tags
+  my ($text) = @_;
+  $text =~ s/</\&lt;/g;
+  $text =~ s/</\&gt;/g;
+  return $text;
+}
+
+sub url {
+  # a very simple URL sanitizer
+  my ($url) = @_;
+  if($url =~ s![^a-zA-Z0-9/_:.-]!!g) {
+    warn "Invalid characcters in url $_[0]";
+  }
+  return $url;
+}
+
+1;
