2946953a575f17b00c4729c51e71072712bfbf5e
Roland Haas <rhaas@illinois.edu>
2019-03-05 07:36:39 -0600

    point (old) php request_testaccount redirect back to current tutorial
    apparently the old link is actually used on websites and used be people

--- request_testaccount.php
+++ request_testaccount.php
@@ -1,3 +1,4 @@
 <?php
-header("Location: https://einsteintoolkit.org/request_testacount.html")
+//header("Location: https://einsteintoolkit.org/request_testacount.html")
+header("Location: https://einsteintoolkit.org/documentation/new-user-tutorial")
 ?>
