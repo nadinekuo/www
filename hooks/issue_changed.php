d946e22337683d701355004ab4f0e8d1430e117b
Steven R. Brandt <sbrandt@cct.lsu.edu>
2019-03-11 16:22:00 -0500

    sender is trac-noreply

--- hooks/issue_changed.php
+++ hooks/issue_changed.php
@@ -61,11 +61,11 @@ if ($hook_uuid != "b1beef0a-aab4-4384-be6e-c635fee232a7") {
   $msg .= "</html>";
 
   if ($subject != "") {
-    $headers  = "From: trac@einsteintoolkit.org\r\n";
+    $headers  = "From: trac-noreply@einsteintoolkit.org\r\n";
     $headers .= "MIME-Version: 1.0\r\n";
     $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
 
-    $email = 'trac-noreply@einsteintoolkit.org';
+    $email = 'trac@einsteintoolkit.org';
     $rc = mail($email, $subject, $msg, $headers);
     echo ("mail sent successfully:".$rc);
 
