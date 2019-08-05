47332f5d16a8db4e562c8452c76890ddf8970315
Steven R. Brandt <sbrandt@cct.lsu.edu>
2019-08-05 16:22:58 -0400

    Update of developer data

--- developers.py
+++ developers.py
@@ -2,7 +2,7 @@
 import re
 
 headers = None
-missing = 0
+missing = {}
 with open("developers.txt","r") as fd:
     for line in fd.readlines():
         cols = line.strip().split(":")
@@ -12,10 +12,15 @@ with open("developers.txt","r") as fd:
             if len(cols) > len(headers):
                 raise Exception("Too many columns:",cols)
             if len(cols) == 4:
-                if not re.match(r'\s*\d{4}(-\d{4}){3}\s*$',cols[3]):
+                if re.match(r'^\s*$',cols[3]) or re.match(r'\s*\w{4}(-\w{4}){3}\s*$',cols[3]):
+                    pass
+                else:
                     raise Exception("Bad ORCID:",cols)
             for i in range(1,len(headers)):
-                if i >= len(cols):
+                if i >= len(cols) or re.match(r'^\s*$',cols[i]):
+                    h = headers[i]
+                    if h not in missing:
+                        missing[h] = 0
                     print("Missing %s for user %s" % (headers[i], cols[0]))
-                    missing += 1
+                    missing[h] += 1
 print("Items missing:",missing)
