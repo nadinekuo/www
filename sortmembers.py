70fedb6549c0b2e2c918f8fc9aafb287e4f9c299
Steven R. Brandt <sbrandt@cct.lsu.edu>
2019-08-07 11:40:48 -0400

    sortmembers.py now detects duplicates

--- sortmembers.py
+++ sortmembers.py
@@ -34,5 +34,45 @@ with open(".members.txt","w") as fd:
         for p in people:
             print(p,file=fd)
         print('',file=fd)
+
+def dist(a,b):
+    d = (len(a)+len(b))/2
+    h = {}
+    for c in a:
+        if c not in h:
+            h[c]=0
+        h[c] += 1
+    for c in b:
+        if c not in h:
+            h[c]=0
+        h[c] -= 1
+    sum = 0
+    for v in h.values():
+        sum += abs(v)
+    return sum/d
+
 os.remove("members.txt")
 os.rename(".members.txt","members.txt")
+
+header = False
+for u1 in univ:
+    for u2 in univ:
+        if u1 != u2:
+            for m1 in members[u1].keys():
+                k1 = re.sub('^\s*http:\S+','',m1)
+                for m2 in members[u2].keys():
+                    if m2 < m1:
+                        continue
+                    k2 = re.sub('^\s*http:\S+','',m2)
+                    d = dist(k1,k2)
+                    if d < .3:
+                        if not header:
+                            print("Possible Duplictes:")
+                            header = True
+                        #print("Dist:",d)
+                        ku1 = re.sub(r'https?://\S+\s*','',u1)
+                        ku2 = re.sub(r'https?://\S+\s*','',u2)
+                        print("  ",k1,"at",ku1)
+                        print("  ",k2,"at",ku2)
+                        print()
+#print(dist("Steve Brandt","Steven R. Brandt"))
