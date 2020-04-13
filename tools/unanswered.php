47c6143600c56e9145c9179bcac02281894bb50d
Roland Haas <rhaas@ncsa.illinois.edu>
2020-04-13 16:39:31 -0500

    unannswered: correct javascript paths

--- tools/unanswered.php
+++ tools/unanswered.php
@@ -1,14 +1,14 @@
 <html lang="en">
 <head>
   <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
-  <script src="head.js" type="text/javascript">
+  <script src="../head.js" type="text/javascript">
   </script>
   <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
   <title>Possible unaswered emails on users mailing list</title>
 </head>
 <body id="index">
   <header>
-    <script src="menu.js" type="text/javascript"></script>
+    <script src="../menu.js" type="text/javascript"></script>
   </header>
   <div class="container">
   <h3>Possible unanswered emails</h3>
@@ -20,7 +20,7 @@
   <div class="container">
     <div class="row">
       <div class="col-xs-12">
-        <script src="footer/footer.js" type="text/javascript"></script>
+        <script src="../footer/footer.js" type="text/javascript"></script>
       </div>
     </div>
   </div>
