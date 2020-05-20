c531344952afe4167115d1aeea923e7413b96fe4
Roland Haas <rhaas@ncsa.illinois.edu>
2020-05-20 08:45:11 -0500

    mk_release_announcements: embedd link to HTML announcement in email

--- about/releases/mk_release_announcements.py
+++ about/releases/mk_release_announcements.py
@@ -49,66 +49,80 @@ with open(sys.argv[1],"r") as fd:
 </html>
         """,file=fw)
 
-def make_text(manual_breaks, fw):
+def make_text(email_target, base_name):
+  manual_breaks = email_target
   textwidth = 72 # maxium allowed length including indentation
-  with open(sys.argv[1], "r", encoding='ascii') as fd:
-      for line in readlines(fd):
-          line = line.strip()
+  url = "https://einsteintoolkit.org/about/releases/%s.html" % os.path.basename(base_name)
+  if(email_target):
+      fwn = base_name + ".email.txt"
+  else:
+      fwn = base_name + ".txt"
+  with open(fwn, "w") as fw:
+      with open(base_name + ".md", "r", encoding='ascii') as fd:
+          lines = readlines(fd)
+          if email_target:
+              # insert a "Click here to see this online" link after title
+              for i in range(len(lines)):
+                    if(re.match(r'^# ', lines[i])):
+                        lines.insert(i+1, '\n')
+                        lines.insert(i+2, 'Click here to see this online:\n')
+                        lines.insert(i+3, '%s\n' % url)
+                        break
 
-          # Ensure there are no weird characters
-          n = 0
-          for ch in line:
-              assert re.match(r'^[=$\+\&`\[\]\(\)\{\}@;,?\./" \t\w:\*\#\'-]+$',ch), line[0:n]+"<"+ch+":"+str(ord(ch))+">"+line[n+1:]
-              n += 1
+          for line in lines:
+              line = line.strip()
 
-          if len(line) == 0:
-              indent = 0
-              linewidth = textwidth
-          elif line[0] == '+':
-              indent = 3
-              linewidth = textwidth - (2*indent+1)
-          elif line[0] == '-':
-              indent = 2
-              linewidth = textwidth - (2*indent+1)
-          elif line[0] == '*':
-              indent = 1
-              linewidth = textwidth - (2*indent+1)
-          else:
-              indent = 0
-              linewidth = textwidth
+              # Ensure there are no weird characters
+              n = 0
+              for ch in line:
+                  assert re.match(r'^[=$\+\&`\[\]\(\)\{\}@;,?\./" \t\w:\*\#\'-]+$',ch), line[0:n]+"<"+ch+":"+str(ord(ch))+">"+line[n+1:]
+                  n += 1
 
-          line = re.sub(r'\[([^\]]*)\]\(([^\)]*)\)',r'\1',line)
-          line = re.sub(r'^#+\s+','',line)
-          line = re.sub(r'`([^`\s]*)`',r'\1',line)
+              if len(line) == 0:
+                  indent = 0
+                  linewidth = textwidth
+              elif line[0] == '+':
+                  indent = 3
+                  linewidth = textwidth - (2*indent+1)
+              elif line[0] == '-':
+                  indent = 2
+                  linewidth = textwidth - (2*indent+1)
+              elif line[0] == '*':
+                  indent = 1
+                  linewidth = textwidth - (2*indent+1)
+              else:
+                  indent = 0
+                  linewidth = textwidth
 
-          sp = ' '*(2*indent-1)
+              line = re.sub(r'\[([^\]]*)\]\(([^\)]*)\)',r'\1',line)
+              line = re.sub(r'^#+\s+','',line)
+              line = re.sub(r'`([^`\s]*)`',r'\1',line)
 
-          if line == '' or not manual_breaks:
-              print(sp,line,sep='',file=fw)
-              continue
-          # The regular expression {0,N} greedily looks
-          # for matches of at most N characters.
-          while True:
-              g = re.match(r'(.{1,%d})(\s+|$)' % (linewidth-len(sp)), line)
-              if not g:
-                  break
-              segment = g.group(0)
-              line = line[g.end(0):]
+              sp = ' '*(2*indent-1)
 
-              # Don't generate an extra newline for
-              # the last blank pattern match.
-              if segment == '':
-                  break
+              if line == '' or not manual_breaks:
+                  print(sp,line,sep='',file=fw)
+                  continue
+              # The regular expression {0,N} greedily looks
+              # for matches of at most N characters.
+              while True:
+                  g = re.match(r'(.{1,%d})(\s+|$)' % (linewidth-len(sp)), line)
+                  if not g:
+                      break
+                  segment = g.group(0)
+                  line = line[g.end(0):]
 
-              # A list gets a different indentation
-              # on the continuation line because we
-              # want to indent past the bullet.
-              print(sp, segment.rstrip(), sep='', file=fw)
-              if indent > 0:
-                  sp = ' '*(2*indent+1)
+                  # Don't generate an extra newline for
+                  # the last blank pattern match.
+                  if segment == '':
+                      break
 
-with open(base_name+".txt","w") as fw:
-    make_text(manual_breaks=False, fw=fw)
+                  # A list gets a different indentation
+                  # on the continuation line because we
+                  # want to indent past the bullet.
+                  print(sp, segment.rstrip(), sep='', file=fw)
+                  if indent > 0:
+                      sp = ' '*(2*indent+1)
 
-with open(base_name+".email.txt","w") as fw:
-    make_text(manual_breaks=True, fw=fw)
+make_text(email_target=False, base_name=base_name)
+make_text(email_target=True, base_name=base_name)
