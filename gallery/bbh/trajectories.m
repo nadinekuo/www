fecf36dc99e17dc47918da6caa7c593eb810a36d
Steven R. Brandt <sbrandt@cct.lsu.edu>
2020-11-24 16:56:14 -0600

    Update the mathematica scripts to specify the Provider (a term used by SimulationTools) and to write out data in a csv.

--- gallery/bbh/trajectories.m
+++ gallery/bbh/trajectories.m
@@ -19,15 +19,15 @@ ProviderPreferences["Waveforms"] = {"MultipoleHDF5"};
 trajs = ReadBHTrajectories[sim];
 relTrajs = trajs[[1]] - trajs[[2]];
 
-trajPlot = 
+trajPlot =
   Framed[ListLinePlot[relTrajs,
-    PlotTheme -> "Detailed", 
-    AspectRatio -> Automatic, LabelStyle -> "Medium", 
+    PlotTheme -> "Detailed",
+    AspectRatio -> Automatic, LabelStyle -> "Medium",
     FrameLabel -> {"(\!\(\*SubscriptBox[\(x\), \(1\)]\)-\!\(\*SubscriptBox[\(x\
-      \), \(2\)]\))/M", 
+      \), \(2\)]\))/M",
       "(\!\(\*SubscriptBox[\(y\), \(1\)]\)-\!\(\*SubscriptBox[\(y\), \
-        \(2\)]\))/M"}, ImageMargins -> 20, Background -> White, ImageSize -> 200, 
-    PlotRange -> {{-1, 1}, {-1, 1}} 12, 
+        \(2\)]\))/M"}, ImageMargins -> 20, Background -> White, ImageSize -> 200,
+    PlotRange -> {{-1, 1}, {-1, 1}} 12,
     PlotStyle -> Directive[Black, AbsoluteThickness[1]]], FrameMargins -> None];
 
 Export[FileNameJoin[{outDir, "trajectories.png"}], Magnify[trajPlot,2]];
